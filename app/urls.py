723a0a230b4acd34715f3b59f0f9b6d9a7482a58
andrekur <kurepin.andre@yandex.ru>
2022-08-03 11:32:06 +0300

    add jwt app

--- app/urls.py
+++ app/urls.py
@@ -38,5 +38,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tasks_app.urls')),
+    path('api/token/', include('jwt_app.urls')),
     path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
